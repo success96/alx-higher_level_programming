a0820c62719b8e062b6621de7eb36d72e5183a0e
Olaosebikan Fawas <61049621+success96@users.noreply.github.com>
2023-09-04 14:46:16 +0100

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++
@@ -1,2 +0,0 @@
---  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT a.title, b.genre_id FROM tv_shows a, tv_show_genres b WHERE b.show_id = a.id ORDER BY a.title ASC, b.genre_id ASC;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++
@@ -1,2 +0,0 @@
--- lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT a.title, b.genre_id FROM tv_shows a LEFT JOIN tv_show_genres b ON a.id = b.show_id WHERE b.genre_id IS NULL ORDER BY a.title ASC, b.genre_id ASC;
